bf4163c9dad6dc5c19ed95ca3fa47d47c0ea179d
kasra <kasra@Admins-iMac.local>
2026-05-31 15:29:36 +0400

    add queue to job for sending email and try and backoff property

--- app/Jobs/SendReservationConfirmation.php
+++ app/Jobs/SendReservationConfirmation.php
@@ -6,11 +6,17 @@ use App\Mail\ReservationConfirmedMail;
 use App\Models\Reservation;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
+use Throwable;
 
 class SendReservationConfirmation implements ShouldQueue
 {
     use Queueable;
+
+    public $tries = 3;
+
+    public $backoff = [10, 30, 60];
 
     public Reservation $reservation;
 
@@ -21,10 +27,19 @@ class SendReservationConfirmation implements ShouldQueue
 
     public function handle(): void
     {
+//        throw new \Exception('Test queue failure');
         $reservation = $this->reservation->load('user', 'book');
 
         Mail::to($reservation->user->email)
             ->send(new ReservationConfirmedMail($reservation));
+    }
+
+    public function failed(Throwable $exception): void
+    {
+        Log::error('Reservation confirmation job failed', [
+            'reservation_id' => $this->reservation->id,
+            'message' => $exception->getMessage(),
+        ]);
     }
 
 }
